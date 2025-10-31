b27e10d2673002fe2725befcb4f4f3ce2a74255c
Park sang min <181554577+qkrtkdals962@users.noreply.github.com>
2025-11-01 00:01:42 +0900

    더미데이터 3개(pdf제외)

---
+++ DB데이터.sql
@@ -0,0 +1,5 @@
+INSERT INTO `char_info` (pdf_name, pdf, score, keyword, ai_summary)
+VALUES
+('c03.pdf', NULL, 73, 'React, Redux, TypeScript, Next.js, Vue.js, JavaScript ES6+, CSS/SASS, 반응형 UI', '홍길동 최고'),
+('c01.pdf', NULL, 56, 'React, Redux, TypeScript, Next.js, Vue.js', '김철수 최고'),
+('c02.pdf', NULL, 32, 'Vue.js, JavaScript ES6+, CSS/SASS', '이영희 최고');
