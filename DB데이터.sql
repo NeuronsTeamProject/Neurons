6281e5daa69356dbfe9c8343e7460fa6a55215eb
Park sang min <181554577+qkrtkdals962@users.noreply.github.com>
2025-11-27 14:30:21 +0900

    더미데이터 수정

--- DB데이터.sql
+++ DB데이터.sql
@@ -1,5 +1,5 @@
-INSERT INTO `char_info` (pdf_name, pdf, score, keyword, ai_summary)
+INSERT INTO `char_info` (pdf_name, job_role, pdf, score, keyword, ai_summary)
 VALUES
-('c03.pdf', NULL, 73, 'React, Redux, TypeScript, Next.js, Vue.js, JavaScript ES6+, CSS/SASS, 반응형 UI', '홍길동 최고'),
-('c01.pdf', NULL, 56, 'React, Redux, TypeScript, Next.js, Vue.js', '김철수 최고'),
-('c02.pdf', NULL, 32, 'Vue.js, JavaScript ES6+, CSS/SASS', '이영희 최고');
+('c01.pdf', '프론트엔드', NULL, 73, 'React, Redux, TypeScript, Next.js, Vue.js, JavaScript ES6+, CSS/SASS, 반응형 UI', '홍길동 최고'),
+('c02.pdf', '백엔드', NULL, 56, 'React, Redux, TypeScript, Next.js, Vue.js', '김철수 최고'),
+('c03.pdf', 'UI/UX', NULL, 32, 'Vue.js, JavaScript ES6+, CSS/SASS', '이영희 최고');
