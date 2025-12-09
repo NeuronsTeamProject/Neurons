da5b4e92c2c08645f4310f2dca915faafbd95855
Park sang min <181554577+qkrtkdals962@users.noreply.github.com>
2025-12-09 23:00:35 +0900

    Delete main directory

--- main/java/com/example/resume/repository/CharInfoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.resume.repository;
-
-import com.example.resume.entity.CharInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CharInfoRepository extends JpaRepository<CharInfo, Integer> {
-}
